4921ed74d88528021fc34186ffb202617ba7fc1b
bswck <bartosz@ilikepython.com>
2025-08-12 15:30:26 +0200

    Remove unnecessary ctypes code

--- new0/compat.py
+++ new0/compat.py
@@ -18,81 +18,14 @@ def decref(op: ctypes._Pointer[PyObject]) -> None:
             )
 
 
-class PyTypeObject(ctypes.Structure):
-    _fields_: ClassVar = [
-        ("tp_dealloc", ctypes.CFUNCTYPE(None, ctypes.POINTER(ctypes.c_void_p))),
-    ]
-
-
 class PyObject(ctypes.Structure):
-    _fields_: ClassVar = [
-        ("ob_refcnt", ctypes.c_int),
-        ("ob_type", ctypes.POINTER(PyTypeObject)),
-    ]
-
-
-class Grammar(ctypes.Structure):
-    _fields_: ClassVar = [
-        ("g_ndfas", ctypes.c_int),
-        ("g_dfa", ctypes.POINTER(ctypes.c_void_p)),
-        ("g_labels", ctypes.c_void_p),
-        ("g_start", ctypes.c_int),
-    ]
+    _fields_: ClassVar = [("ob_refcnt", ctypes.c_int), ("ob_type", ctypes.c_void_p)]
 
 
 class Python0API(ctypes.CDLL):
     """A ctypes wrapper for the Python 0.9.1 shared library."""
 
     E_DONE: Final = 16
-
-    @cached_property
-    def gram(self) -> Grammar:
-        return Grammar.in_dll(self, "gram")
-
-    @cached_property
-    def initall(self) -> ctypes._FuncPointer:
-        self._initall.argtypes = []
-        self._initall.restype = None
-        return self._initall
-
-    @cached_property
-    def parse_string(self) -> ctypes._FuncPointer:
-        self._parse_string.argtypes = [
-            ctypes.c_char_p,
-            ctypes.c_int,
-            ctypes.POINTER(ctypes.c_void_p),
-        ]
-        self._parse_string.restype = ctypes.c_int
-        return self._parse_string
-
-    @cached_property
-    def add_module(self) -> ctypes._FuncPointer:
-        self._add_module.argtypes = [ctypes.c_char_p]
-        self._add_module.restype = ctypes.c_void_p
-        return self._add_module
-
-    @cached_property
-    def getmoduledict(self) -> ctypes._FuncPointer:
-        self._getmoduledict.argtypes = [ctypes.c_void_p]
-        self._getmoduledict.restype = ctypes.c_void_p
-        return self._getmoduledict
-
-    @cached_property
-    def eval_node(self) -> ctypes._FuncPointer:
-        self._eval_node.argtypes = [
-            ctypes.c_void_p,
-            ctypes.c_char_p,
-            ctypes.c_void_p,
-            ctypes.c_void_p,
-        ]
-        self._eval_node.restype = ctypes.c_void_p
-        return self._eval_node
-
-    @cached_property
-    def print_error(self) -> ctypes._FuncPointer:
-        self._print_error.argtypes = []
-        self._print_error.restype = None
-        return self._print_error
 
     @cached_property
     def dict(self) -> ctypes.c_void_p:
@@ -114,7 +47,10 @@ def run_code_string(
         return
 
     result = python0.eval_node(
-        node, filename.encode("utf-8"), python0.dict, python0.dict
+        node,
+        filename.encode("utf-8"),
+        python0.dict,
+        python0.dict,
     )
 
     if not result:
